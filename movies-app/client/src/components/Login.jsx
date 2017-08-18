1d0791ee4c43b039c8c5b5e8abee593e7df02f7d
Amidou Kante <kamdiou@gmail.com>
2017-08-18 11:46:14 -0400

    added files and login js

--- movies-app/client/src/components/Login.jsx
+++ movies-app/client/src/components/Login.jsx
@@ -21,8 +21,8 @@ class Login extends Component {
         return(
             <div className="loginForm">
                 <form onSubmit={(e)=>this.props.handleSubmit(e, this.state.username, this.state.password)}>
-                    <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange}/>
-                    <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange}/>
+                    <input type="text" required="true" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange}/>
+                    <input type="password" required="true" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange}/>
                     <input type="submit" value="LogIn" onClick={this.handleSubmit} />
                 </form>
             </div>
---
+++ movies-app/client/src/components/Register.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInputChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+    
+    render(){
+        return(
+            <div className="registerForm">
+                <form onSubmit={(e)=>this.props.handleSubmit(e, this.state.username, this.state.password)}>
+                    <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange}/>
+                    <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange}/>
+                    <input type="submit" value="LogIn" onClick={this.handleSubmit} />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register;
