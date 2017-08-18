9f40372dacd6e8950c6d4d01f5cd6436be046e6e
Amidou Kante <kamdiou@gmail.com>
2017-08-18 11:40:56 -0400

    handleInputChange

--- movies-app/client/src/components/Login.jsx
+++ movies-app/client/src/components/Login.jsx
@@ -10,16 +10,20 @@ class Login extends Component {
     }
 
     handleInputChange = (e) => {
-
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value
+        })
     }
     
     render(){
         return(
             <div className="loginForm">
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="username" placeholder="username" onChange={this.handleInputChange}/>
-                    <input type="password" name="password" placeholder="password" onChange={this.handleInputChange}/>
-                    <input type="submit" value="submit" onClick={this.handleSubmit} />
+                    <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange}/>
+                    <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange}/>
+                    <input type="submit" value="LogIn" onClick={this.handleSubmit} />
                 </form>
             </div>
         )
