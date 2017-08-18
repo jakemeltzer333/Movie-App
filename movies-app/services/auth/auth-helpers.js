ede82c314e4da7485019f8417c7a5ff230397a89
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-18 11:40:56 -0400

    create services directory and initialize auth helpers file

---
+++ movies-app/services/auth/auth-helpers.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcryptjs');
+const User = require('../../models/user');
+
+function comparePass(userPassword, databasePassword) {
+  return bcrypt.compareSync(userPassword, databasePassword);
+}
+
