312a1dc3469d95bcefa2cdcb934b92db9ca9ca30
Karen Baque <k.baque@aol.com>
2017-08-18 10:56:44 -0400

    import header, footer and home

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
 class App extends Component {
    constructor() {
       super()
@@ -12,13 +16,34 @@ class App extends Component {
           currentMovieId: null,
           movieData: null,
       }
+
+      this.setPage = this.setPage.bind(this);
    }
+
+ setPage(page) {
+  console.log('click');
+   this.setState({
+    currentPage: page,
+   })
+ }
+
+ decideWhichPage() {
+  switch(this.state.currentPage) {
+    case 'home';
+     return <Home />;
+     break;
+     default:
+     break;
+   }
+ }
 
 
   render() {
     return (
       <div className="App">
-
+       <Header setPage={this.setPage} />
+        {this.decideWhichPage()}
+       <Footer />
       </div>
     );
   }
